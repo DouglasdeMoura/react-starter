0506e9e54f0d1d0011dd259cda2e340e82343e6f
Douglas Moura <douglas.ademoura@gmail.com>
2022-05-03 20:45:39 -0300

    chore: upgrade to React 18

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,8 @@
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 
 import { App } from './app'
 
-ReactDOM.render(<App />, document.getElementById('app'))
+const container = document.getElementById('app')
+const root = createRoot(container!)
+
+root.render(<App />)
