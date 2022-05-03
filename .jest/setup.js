62b01e8080c329dd95198972eeaa82ea19a09483
Douglas Moura <douglas.ademoura@gmail.com>
2022-05-03 20:53:11 -0300

    chore: add @storybook/testing-react

--- .jest/setup.js
+++ .jest/setup.js
@@ -1,3 +1,7 @@
 import * as React from 'react'
+import { setGlobalConfig } from '@storybook/testing-react';
+import * as globalStorybookConfig from '../.storybook/preview';
+
+setGlobalConfig(globalStorybookConfig);
 
 global.React = React
