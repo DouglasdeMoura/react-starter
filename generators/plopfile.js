1bdd8b905d9754719b7f4a60c837fe868d6d5927
Douglas Moura <douglas.ademoura@gmail.com>
2022-05-03 20:40:04 -0300

    chore: update template files

--- generators/plopfile.js
+++ generators/plopfile.js
@@ -12,6 +12,11 @@ module.exports = function (plop) {
       {
         type: 'add',
         path: '../src/atoms/{{kebabCase name}}/index.tsx',
+        templateFile: 'templates/index.tsx.hbs',
+      },
+      {
+        type: 'add',
+        path: '../src/atoms/{{kebabCase name}}/{{kebabCase name}}.tsx',
         templateFile: 'templates/component.tsx.hbs',
       },
       {
@@ -25,32 +30,38 @@ module.exports = function (plop) {
         templateFile: 'templates/test.tsx.hbs',
       },
     ],
-  }),
-    plop.setGenerator('component', {
-      description: 'Create an component',
-      prompts: [
-        {
-          type: 'input',
-          name: 'name',
-          message: 'What is the name of your component?',
-        },
-      ],
-      actions: [
-        {
-          type: 'add',
-          path: '../src/components/{{kebabCase name}}/index.tsx',
-          templateFile: 'templates/component.tsx.hbs',
-        },
-        {
-          type: 'add',
-          path: '../src/components/{{kebabCase name}}/{{kebabCase name}}.stories.tsx',
-          templateFile: 'templates/stories.tsx.hbs',
-        },
-        {
-          type: 'add',
-          path: '../src/components/{{kebabCase name}}/{{kebabCase name}}.test.tsx',
-          templateFile: 'templates/test.tsx.hbs',
-        },
-      ],
-    })
+  })
+
+  plop.setGenerator('component', {
+    description: 'Create an component',
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is the name of your component?',
+      },
+    ],
+    actions: [
+      {
+        type: 'add',
+        path: '../src/components/{{kebabCase name}}/index.tsx',
+        templateFile: 'templates/index.tsx.hbs',
+      },
+      {
+        type: 'add',
+        path: '../src/components/{{kebabCase name}}/{{kebabCase name}}.tsx',
+        templateFile: 'templates/component.tsx.hbs',
+      },
+      {
+        type: 'add',
+        path: '../src/components/{{kebabCase name}}/{{kebabCase name}}.stories.tsx',
+        templateFile: 'templates/stories.tsx.hbs',
+      },
+      {
+        type: 'add',
+        path: '../src/components/{{kebabCase name}}/{{kebabCase name}}.test.tsx',
+        templateFile: 'templates/test.tsx.hbs',
+      },
+    ],
+  })
 }
