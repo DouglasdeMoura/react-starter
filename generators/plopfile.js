ab86518b1ce712fbe375199ba8de911eca41d1d9
Douglas Moura <douglas.ademoura@gmail.com>
2024-07-23 22:01:43 -0300

    chore: remove atom option

--- generators/plopfile.js
+++ generators/plopfile.js
@@ -1,37 +1,4 @@
 module.exports = function (plop) {
-  plop.setGenerator('atom', {
-    description: 'Create an atom',
-    prompts: [
-      {
-        type: 'input',
-        name: 'name',
-        message: 'What is the name of your atom?',
-      },
-    ],
-    actions: [
-      {
-        type: 'add',
-        path: '../src/atoms/{{kebabCase name}}/index.tsx',
-        templateFile: 'templates/index.tsx.hbs',
-      },
-      {
-        type: 'add',
-        path: '../src/atoms/{{kebabCase name}}/{{kebabCase name}}.tsx',
-        templateFile: 'templates/component.tsx.hbs',
-      },
-      {
-        type: 'add',
-        path: '../src/atoms/{{kebabCase name}}/{{kebabCase name}}.stories.tsx',
-        templateFile: 'templates/stories.tsx.hbs',
-      },
-      {
-        type: 'add',
-        path: '../src/atoms/{{kebabCase name}}/{{kebabCase name}}.spec.tsx',
-        templateFile: 'templates/spec.tsx.hbs',
-      },
-    ],
-  })
-
   plop.setGenerator('component', {
     description: 'Create an component',
     prompts: [
